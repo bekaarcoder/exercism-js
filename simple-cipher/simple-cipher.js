2c781058f479731b55faa9f3aac1f90fe6567c77
Basant <shashank.basant@accenture.com>
2017-08-31 10:54:41 +0530

    simple cipher

--- simple-cipher/simple-cipher.js
+++ simple-cipher/simple-cipher.js
@@ -1,25 +1,33 @@
 var Cipher = function(input) {
-	var keyStr = input.match(/[0-9A-Z]+/g);
+	if(input == "" || input.match(/[0-9A-Z]+/)) {
+		throw new Error('Bad key');
+	} else {
+		this.key = input;
+	}
+	/*var keyStr = input.match(/[0-9A-Z]+/g);
 	if(keyStr) {
 		throw new Error('Bad key');
 	} else if(input == "") {
 		this.key = "dddddddddddddddddd";
 		throw new Error('Bad key');
-	}
+	}*/
 }
 
-// Cipher.prototype.key = "dddddddddddddddddd";
+Cipher.prototype.key = "dddddddddddddddddd";
 
 Cipher.prototype.encode = function(input) {
-	var alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"];
+	var alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n","o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 	var newInput = "";
 	var inputLength = input.length;
 	// var cipherKey = new Cipher();
-	var key = this.key.substr(0, 10);
+	var key = this.key;
 	for(var i=0; i<inputLength; i++) {
 		var keyIndex = alphabets.indexOf(key[i]);
 		var inputIndex = alphabets.indexOf(input[i]);
 		var newIndex = keyIndex + inputIndex;
+		if(newIndex >= (alphabets.length)) {
+			newIndex = newIndex - (alphabets.length);
+		}
 		newInput += alphabets[newIndex]; 
 	}
 	Cipher.prototype.encodedData = newInput;
@@ -27,7 +35,7 @@ Cipher.prototype.encode = function(input) {
 }
 
 Cipher.prototype.decode = function(input) {
-	var alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"];
+	var alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n","o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 	var output = "";
 	var inputLength = input.length;
 	// var cipherKey = new Cipher();
