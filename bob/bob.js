8db8ee32edfbb50dbc78046e454adf3b77763cd1
Shashank <shashank_smaty@yahoo.co.in>
2017-09-11 09:03:55 +0530

    Bob completed

--- bob/bob.js
+++ bob/bob.js
@@ -4,8 +4,12 @@ var Bob = function() {
 
 Bob.prototype.hey = function(input) {
 	var askBob = input.trim();
-	if(askBob.indexOf('?') != -1 && askBob.indexOf('?') === askBob.length - 1) {
+	if(askBob.toUpperCase() == askBob && askBob !== "" && /[A-Z]/g.test(askBob.toUpperCase()) == true) {
+		return "Whoa, chill out!";
+	} else if(askBob.indexOf('?') != -1 && askBob.indexOf('?') === askBob.length - 1 && askBob !== "") {
 		return 'Sure.';
+	} else if(askBob === "") {
+		return "Fine. Be that way!";
 	} else {
 		return 'Whatever.';
 	}
